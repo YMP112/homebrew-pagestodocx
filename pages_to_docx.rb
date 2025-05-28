04205ffe33872e014ce3cce36d655b1e42e148e3
יעקב מ. פינס <74414287+YMP112@users.noreply.github.com>
2025-05-29 00:48:59 +0300

    Update pages_to_docx.rb

--- pages_to_docx.rb
+++ pages_to_docx.rb
@@ -4,6 +4,7 @@ class PagesToDocx < Formula
   url "https://github.com/YMP112/PagesToDocx-AppleApp/releases/download/PagesToDocx/PagesToDocx.zip"
   sha256 "c6a32791cd7087a54fce9f129cb0830ec39e5427854abdfd5bcfd9c024a55f64"
   version "2.0"
+  app "PagesToDocx.app"
 
   depends_on :macos
 
